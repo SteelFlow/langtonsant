48354d3ee7f980d2a3771e729ea05c52b82e5356
SteelFlow <staale.stromme@gmail.com>
2019-07-02 20:25:50 +0200

    logic refactoring

--- LangtonsAnt.fs
+++ LangtonsAnt.fs
@@ -10,22 +10,35 @@ type Pos = {x : int; y : int}
 
 type State = { pos: Pos; dir: Direction; matrix: bool [,] }
 
-let next (state : State) =
-    let pos = state.pos
-    let cell = Array2D.get state.matrix pos.x pos.y
-    let (nextPost, nextDir) =
-        match (state.dir, cell) with
-        | North, false -> ({pos with x = pos.x + 1}, East)
-        | East, false -> ({pos with y = pos.y + 1}, South)
-        | South, false -> ({pos with x = pos.x - 1}, West)
-        | West, false -> ({pos with y = pos.y - 1}, North)
-        
-        | North, true -> ({pos with x = pos.x - 1}, West)
-        | East, true -> ({pos with y = pos.y - 1}, North)
-        | South, true -> ({pos with x = pos.x + 1}, East)
-        | West, true -> ({pos with y = pos.y + 1}, South)
-    
-    let newMatrix = Array2D.copy state.matrix
-    Array2D.set newMatrix pos.x pos.y (not cell) // Tror denne er mutable
+let cellValue state = Array2D.get state.matrix state.pos.x state.pos.y
 
-    { pos = nextPost; dir = nextDir; matrix = newMatrix }
+let private turn state =
+    match (state.dir, (cellValue state)) with
+    | North, false -> {state with dir = East}
+    | East, false -> {state with dir = South}
+    | South, false -> {state with dir = West}
+    | West, false -> {state with dir = North}
+    
+    | North, true -> {state with dir = West}
+    | East, true -> {state with dir = North}
+    | South, true -> {state with dir = East}
+    | West, true -> {state with dir = South}
+
+let private switch state =
+    let currentCell = cellValue state
+    let newMatrix = Array2D.copy state.matrix
+    Array2D.set newMatrix state.pos.x state.pos.y (not currentCell)
+    {state with matrix = newMatrix}
+
+let private move state =
+    match state.dir with
+    | West -> {state with pos = {x = state.pos.x - 1; y = state.pos.y}}
+    | North -> {state with pos = {x = state.pos.x; y = state.pos.y - 1}}
+    | East -> {state with pos = {x = state.pos.x + 1; y = state.pos.y}}
+    | South -> {state with pos = {x = state.pos.x; y = state.pos.y + 1}}
+
+let next (state : State) =
+    state
+    |> turn
+    |> switch
+    |> move
