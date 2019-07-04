25474b3c1de8166a4a4da59e3d47c9727cc32131
SteelFlow <staale.stromme@gmail.com>
2019-07-04 10:45:57 +0200

    Use Colors instead of bool for cell state

--- LangtonsAnt.fs
+++ LangtonsAnt.fs
@@ -8,26 +8,33 @@ type Direction =
 
 type Pos = {x : int; y : int}
 
-type State = { pos: Pos; dir: Direction; matrix: bool [,] }
+type CellColor = White | Black
 
-let cellValue state = Array2D.get state.matrix state.pos.x state.pos.y
+type State = { pos: Pos; dir: Direction; matrix: CellColor [,] }
+
+let private getCellColor state = Array2D.get state.matrix state.pos.x state.pos.y
+
+let private swapCellColor color =
+    match color with
+    | White -> Black
+    | Black -> White
 
 let private turn state =
-    match (state.dir, (cellValue state)) with
-    | North, false -> {state with dir = East}
-    | East, false -> {state with dir = South}
-    | South, false -> {state with dir = West}
-    | West, false -> {state with dir = North}
+    match (state.dir, (getCellColor state)) with
+    | North, White -> {state with dir = East}
+    | East, White -> {state with dir = South}
+    | South, White -> {state with dir = West}
+    | West, White -> {state with dir = North}
     
-    | North, true -> {state with dir = West}
-    | East, true -> {state with dir = North}
-    | South, true -> {state with dir = East}
-    | West, true -> {state with dir = South}
+    | North, Black -> {state with dir = West}
+    | East, Black -> {state with dir = North}
+    | South, Black -> {state with dir = East}
+    | West, Black -> {state with dir = South}
 
 let private switch state =
-    let currentCell = cellValue state
+    let currentCell = getCellColor state
     let newMatrix = Array2D.copy state.matrix
-    Array2D.set newMatrix state.pos.x state.pos.y (not currentCell)
+    Array2D.set newMatrix state.pos.x state.pos.y (swapCellColor currentCell)
     {state with matrix = newMatrix}
 
 let private move state =
--- Tests.fs
+++ Tests.fs
@@ -3,14 +3,14 @@ module LangtonsAntTests
 open Expecto
 open LangtonsAnt
 
-let trueMatrix = (Array2D.create 3 3 true)
-let falseMatrix = (Array2D.create 3 3 false)
-let init = { pos = {x = 1; y = 1}; dir = West; matrix = falseMatrix }
+let blackMatrix = (Array2D.create 3 3 Black)
+let whiteMatrix = (Array2D.create 3 3 White)
+let init = { pos = {x = 1; y = 1}; dir = West; matrix = whiteMatrix }
 
 [<Tests>]
 let directionTests =
     testList "Direction tests" [
-        testList "Call next on false cell - should turn 90° right" [
+        testList "Call next on White cell - should turn 90° right" [
             test "West -> North" {
                 let state = {init with dir = West} 
                 Expect.equal (next state).dir North "Direction should be North"
@@ -32,25 +32,25 @@ let directionTests =
             }    
         ]
 
-        testList "Call next on true cell - should turn 90° left" [
+        testList "Call next on Black cell - should turn 90° left" [
             test "West -> South" {
-                let state = {init with dir = West; matrix = trueMatrix} 
+                let state = {init with dir = West; matrix = blackMatrix} 
                 Expect.equal (next state).dir South "Direction should be South"
             }
 
             test "South -> East" {
-                let state = {init with dir = South; matrix = trueMatrix} 
+                let state = {init with dir = South; matrix = blackMatrix} 
                 Expect.equal (next state).dir East "Direction should be East"
             }
 
             test "East -> North" {
-                let state = {init with dir = East; matrix = trueMatrix} 
+                let state = {init with dir = East; matrix = blackMatrix} 
                 let result = next state
                 Expect.equal result.dir North "Direction should be North"
             }
 
             test "North -> West" {
-                let state = {init with dir = North; matrix = trueMatrix} 
+                let state = {init with dir = North; matrix = blackMatrix} 
                 let result = next state
                 Expect.equal result.dir West "Direction should be West"
             }    
@@ -60,7 +60,7 @@ let directionTests =
 [<Tests>]
 let movementTests = 
     testList "Movement tests" [
-        testList "Call next on false cell - should move one to the right" [
+        testList "Call next on White cell - should move one to the right" [
             test "West -> Move North" {
                 let state = {init with dir = West} 
                 Expect.equal (next state).pos { x=1; y=0 } "Sould move north"
@@ -82,24 +82,24 @@ let movementTests =
             }    
         ]
 
-        testList "Call next on true cell - should turn 90° left" [
+        testList "Call next on Black cell - should turn 90° left" [
             test "West -> South" {
-                let state = {init with dir = West; matrix = trueMatrix} 
+                let state = {init with dir = West; matrix = blackMatrix} 
                 Expect.equal (next state).pos { x=1; y=2 } "Should move South"
             }
 
             test "South -> East" {
-                let state = {init with dir = South; matrix = trueMatrix} 
+                let state = {init with dir = South; matrix = blackMatrix} 
                 Expect.equal (next state).pos { x=2; y=1 } "Should move East"
             }
 
             test "East -> North" {
-                let state = {init with dir = East; matrix = trueMatrix} 
+                let state = {init with dir = East; matrix = blackMatrix} 
                 Expect.equal (next state).pos { x=1; y=0 } "Should move North"
             }
 
             test "North -> West" {
-                let state = {init with dir = North; matrix = trueMatrix} 
+                let state = {init with dir = North; matrix = blackMatrix} 
                 Expect.equal (next state).pos { x=0; y=1 } "Should move West"
             }    
         ]
@@ -108,15 +108,15 @@ let movementTests =
 [<Tests>]
 let colorSwitchTests =
     testList "Color switch tests" [
-        test "Call next on false cell - should set cell to true" {
+        test "Call next on White cell - should set color to Black" {
             let state = init
             let nextState = next state
-            Expect.equal (Array2D.get nextState.matrix 1 1) true "Previous state position should be true" 
+            Expect.equal (Array2D.get nextState.matrix 1 1) Black "Previous state position should be Black" 
         }
 
-        test "Call next on true cell - should set cell to false" {
-            let state = {init with matrix = trueMatrix}
+        test "Call next on Black cell - should set color to White" {
+            let state = {init with matrix = blackMatrix}
             let nextState = next state
-            Expect.equal (Array2D.get nextState.matrix 1 1) false "Previous state position should be false" 
+            Expect.equal (Array2D.get nextState.matrix 1 1) White "Previous state position should be White" 
         }
     ]
